b687b9a83b6edf8a901a5689064f1a37f32f0e96
1georgmoscow-cmd <1georg.moscow@gmail.com>
2026-04-23 19:50:44 +0300

    Update bot_factory.py

--- app/core/bot_factory.py
+++ app/core/bot_factory.py
@@ -1,16 +1,9 @@
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.client.session.aiohttp import AiohttpSession
 
-
-def create_bot(token: str):
-    session = AiohttpSession(
-        proxy="socks5://127.0.0.1:9150"
-    )
-
+def create_bot(token: str) -> Bot:
     return Bot(
         token=token,
-        session=session,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
